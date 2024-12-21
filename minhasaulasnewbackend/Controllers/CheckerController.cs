7659f3e76332561e6e09c244bb931fcebc435611
Well-199 <wellington.marunaka@gmail.com>
2024-12-21 11:45:49 -0300

    adicionado a rota /checker

---
+++ minhasaulasnewbackend/Controllers/CheckerController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using minhasaulasnewbackend.Models;
+
+namespace minhasaulasnewbackend.Controllers
+{
+    [ApiController]
+    public class CheckerController(MinhasaulasContext context) : Controller
+    {
+        private readonly MinhasaulasContext _context = context;
+
+        [HttpGet("checker")]
+        public IActionResult Checker()
+        {
+            try
+            {
+                var user = _context.Usuarios.Find(5);
+                if (user == null) 
+                {
+                    return NotFound(new { data=false, server="server is not working" });
+                }
+                return Ok(new { data=true, server="server running successfully" });
+            }
+            catch (Exception ex) 
+            {
+                BadRequest(ex.Message);
+            }
+            
+            return Ok(true);
+        }
+    }
+}
